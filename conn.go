817ab046682635f0def026374630a0a482909504
lidian-runner <lidian@agi7.ai>
2025-09-30 15:41:44 +0800

    update

--- conn.go
+++ conn.go
@@ -671,9 +671,6 @@ func (c *Conn) processHandshakePacket(pkt *packet, dtlsHandshake *handshake.Hand
 
 		if pkt.shouldEncrypt {
 			var err error
-
-			fmt.Println("use cipherSuite handleshake:", c.state.cipherSuite)
-
 			rawPacket, err = c.state.cipherSuite.Encrypt(pkt.record, rawPacket)
 			if err != nil {
 				return nil, err
